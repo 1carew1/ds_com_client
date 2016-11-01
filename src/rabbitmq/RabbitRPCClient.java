392385650ad33e183aeaee1016b66317f0855d28
Colm Carew <colm.carew@errigal.com>
2016-11-01 14:31:59 +0000

    Adding Jars

---
+++ src/rabbitmq/RabbitRPCClient.java
@@ -0,0 +1,73 @@
+package rabbitmq;
+/***************************************************************
+ * Copyright (c) 2016 Errigal Inc.
+ *
+ * This software is the confidential and proprietary information
+ * of Errigal, Inc.  You shall not disclose such confidential
+ * information and shall use it only in accordance with the
+ * license agreement you entered into with Errigal.
+ *
+ *************************************************************** */
+
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import java.util.UUID;
+
+import java.util.UUID;
+
+/**
+ * Created by Colm Carew on 01/11/2016.
+ */
+public class RabbitRPCClient {
+
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+    private String replyQueueName;
+    private QueueingConsumer consumer;
+
+    public RabbitRPCClient() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPassword("admin");
+        factory.setUsername("admin");
+        factory.setVirtualHost("admin");
+
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        replyQueueName = channel.queueDeclare().getQueue();
+        consumer = new QueueingConsumer(channel);
+        channel.basicConsume(replyQueueName, true, consumer);
+    }
+
+    public String call(String message) throws Exception {
+        String response = null;
+        String corrId = UUID.randomUUID().toString();
+        BasicProperties props = new BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", requestQueueName, props, message.getBytes("UTF-8"));
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody(),"UTF-8");
+                break;
+            }
+        }
+
+        return response;
+    }
+
+    public void close() throws Exception {
+        connection.close();
+    }
+}
