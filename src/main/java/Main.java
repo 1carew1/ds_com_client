254e5e6f9ddc1db179acc374e4a9c427df9fc221
Colm Carew <colm.carew@errigal.com>
2016-11-03 23:13:48 +0000

    Adding Writing results to MySQL DB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -33,9 +37,11 @@ public class Main {
             e.printStackTrace();
         }
         Date restEnd = new Date();
-        System.out.println("Total time to Run Rest  : " + dateDiffMillSec(restEnd, restStart) + " ms");
+        Long restTimeDiffmS = dateDiffMillSec(restEnd, restStart);
+        System.out.println("Total time to Run Rest  : " + restTimeDiffmS + " ms");
         System.out.println("");
         //Rest Stop
+        writeResultsToDB("REST", restTimeDiffmS);
 
 
         // File Read Start
@@ -49,13 +55,14 @@ public class Main {
             e.printStackTrace();
         }
         Date fileEnd = new Date();
-        System.out.println("Total time to Read File  : " + dateDiffMillSec(fileEnd, fileStart) + " ms");
+        Long fileTime = dateDiffMillSec(fileEnd, fileStart);
+        System.out.println("Total time to Read File  : " + fileTime + " ms");
         System.out.println("");
         // File read end
+        writeResultsToDB("FILE", fileTime);
 
         //Corba Start
         Date corbaStart = new Date();
-
         try {
             ORB orb = ORB.init(args, null);
             org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
@@ -70,9 +77,11 @@ public class Main {
         }
 
         Date corbaEnd = new Date();
-        System.out.println("Total time to Run Corba  : " + dateDiffMillSec(corbaEnd, corbaStart) + " ms");
+        Long corbaTime = dateDiffMillSec(corbaEnd, corbaStart);
+        System.out.println("Total time to Run Corba  : " + corbaTime + " ms");
         System.out.println("");
         //Corba End
+        writeResultsToDB("CORBA", corbaTime);
 
         //Java RMI Start
         Date rmiStart = new Date();
@@ -86,9 +95,11 @@ public class Main {
             e.printStackTrace();
         }
         Date rmiEnd = new Date();
-        System.out.println("Total time to Run RMI  : " + dateDiffMillSec(rmiEnd, rmiStart) + " ms");
+        Long rmiTime = dateDiffMillSec(rmiEnd, rmiStart);
+        System.out.println("Total time to Run RMI  : " + rmiTime + " ms");
         System.out.println("");
         // Java RMI End
+        writeResultsToDB("RMI", rmiTime);
 
         // Rabbit RPC Start
         Date rabbitStart = new Date();
@@ -96,17 +107,22 @@ public class Main {
             RabbitRPCClient rabbitRPCClient = new RabbitRPCClient();
             String response = rabbitRPCClient.call("Give me an Object");
             ComplextRMIObject complextRMIObject = jsonStringToComplexObj(response);
-
+            rabbitRPCClient.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
         Date rabbitEnd = new Date();
-        System.out.println("Total time to Run RabbitMq  : " + dateDiffMillSec(rabbitEnd, rabbitStart) + " ms");
+        Long rabbitTime = dateDiffMillSec(rabbitEnd, rabbitStart);
+        System.out.println("Total time to Run RabbitMq  : " + rabbitTime + " ms");
         System.out.println("");
         //Rabbit RPC End
+        writeResultsToDB("RABBITMQ", rabbitTime);
 
-
-        exit(1);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -126,5 +142,46 @@ public class Main {
 
     public static void printComplextObj(ComplextRMIObject complextRMIObject) {
         System.out.println(new Date().toString() + " : " + complextRMIObject.getId() + " " + complextRMIObject.getName() + " " + complextRMIObject.getPhone());
+    }
+
+    public static void writeResultsToDB(String type, Long runtime) {
+        /* Create Table for DB Table
+
+          CREATE TABLE data_results (
+          id int(10) unsigned NOT NULL AUTO_INCREMENT,
+          date datetime DEFAULT NULL,
+          type varchar(32) DEFAULT NULL,
+          runtime int(11) DEFAULT NULL,
+          PRIMARY KEY (`id`)
+         */
+        try {
+            // create a mysql database connection
+            String myDriver = "com.mysql.jdbc.Driver";
+            String myUrl = "jdbc:mysql://localhost/distributed";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "pacemaker", "writer");
+
+            // create a sql date object so we can use it in our INSERT statement
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+
+            // the mysql insert statement
+            String query = " insert into distributed.data_results(date, type, runtime)"
+                    + " values (now(), ?, ?)";
+
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, type);
+            preparedStmt.setLong(2, runtime);
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
